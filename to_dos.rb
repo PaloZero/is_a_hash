c3caa6ea68d02c76316951278930171f33a6f733
Pablo Oseroff <palozero14@gmail.com>
2018-03-29 20:57:21 -0300

    Implemented initialize method in to_dos.rb

--- to_dos.rb
+++ to_dos.rb
@@ -1,17 +1,21 @@
 class ToDos
-  @to_dos
-  @current_id
-  @current_date
-  @current_group
+
+  # Called when creating new instance of class ToDos.
+  # Creates @to_dos as SortedSet.
+  # Sets @current_id to 0.
+  # Sets @current_group and @current date to nil in order not to be confused with other values.
 
   def initialize
-
+    @to_dos = SortedSet.new
+    @current_id = 0
+    @current_date = nil
+    @current_group = nil
   end
 
   def list
 
   end
-  
+
   def add
 
   end
@@ -29,7 +33,7 @@ class ToDos
   end
 
   def next_id
-
+    @current_id += 1
   end
 
   def set_date
