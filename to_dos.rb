cc032ad8c3d91cd43caf85588f0b0ed7e000bdde
Pablo Oseroff <palozero14@gmail.com>
2018-03-29 20:39:51 -0300

    Added methods' prototypes in to_dos.rb

--- to_dos.rb
+++ to_dos.rb
@@ -1,3 +1,42 @@
 class ToDos
+  @to_dos
+  @current_id
+  @current_date
+  @current_group
 
+  def initialize
+
+  end
+
+  def list
+
+  end
+  
+  def add
+
+  end
+
+  def find
+
+  end
+
+  def find_by_id
+
+  end
+
+  def archive
+
+  end
+
+  def next_id
+
+  end
+
+  def set_date
+
+  end
+
+  def set_group
+
+  end
 end
